b62604e507961dd2f0117644c36e4a82202feb60
Özkan Pakdil <ozkan.pakdil@red-gate.com>
2024-10-30 17:44:52 +0000

    for https://rancherdesktop.io/ and containerd , changed docker commands to nerdctl

--- postgres16-ubuntu/setup.ps1
+++ postgres16-ubuntu/setup.ps1
@@ -1,4 +1,4 @@
-docker build --no-cache  -t postgresubuntu16 .
-docker run -dit --name postgresubuntu16 -p 5432:5432 -p 22:22 postgresubuntu16
-docker cp postgresubuntu16:/root/.ssh/id_rsa ./root.key
+nerdctl build --no-cache  -t postgresubuntu16 .
+nerdctl run -dt --name postgresubuntu16 -p 5432:5432 -p 22:22 postgresubuntu16
+nerdctl cp postgresubuntu16:/root/.ssh/id_rsa ./root.key
 ssh -i root.key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@localhost
--- postgres17-ubuntu/setup.ps1
+++ postgres17-ubuntu/setup.ps1
@@ -1,4 +1,4 @@
-docker build --no-cache  -t postgresubuntu17 .
-docker run -dit --name postgresubuntu17 -p 5432:5432 -p 22:22 postgresubuntu17
-docker cp postgresubuntu17:/root/.ssh/id_rsa ./root.key
+nerdctl build --no-cache  -t postgresubuntu17 .
+nerdctl run -dt --name postgresubuntu17 -p 5432:5432 -p 22:22 postgresubuntu17
+nerdctl cp postgresubuntu17:/root/.ssh/id_rsa ./root.key
 ssh -i root.key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@localhost
